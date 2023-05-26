c979272cb480412925c415a7fcd7da4251b0bae5
ishaqahmad41455 <103864675+ishaqahmad41455@users.noreply.github.com>
2023-05-26 16:23:40 +0500

    initial commit

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    # print(MEDIA_URL)
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
